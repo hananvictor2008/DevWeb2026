898fb8f416ccc9897479d13bf21327c874c5f2d0
hananvictor2008 <hananvictor0@gmail.com>
2026-04-12 19:58:59 -0300

    Pedidos json

--- trabalho031126/func.php
+++ trabalho031126/func.php
@@ -1,8 +1,24 @@
 <?php
 
 declare(strict_types=1);
-function resposta(array|null $msg, int $codigo):void {
+function resposta(array|null $info, int $cod):void {
+    http_response_code($cod);
+    die (json_encode($info));
+}
 
+function cacularTotal(float ...$n): float{
+    $total = 0;
+    foreach ($n as $valor) {
+        $total += $valor;
+    }
+    return $total;
+}
+
+function validar(string $pedido):string{
+    if (!$pedido) resposta(["erro" => "Problema de conversão com JSON"], 400);
+    if (!isset($pedido["nome"], $pedido["nota1"], $pedido["nota2"])) resposta(["erro" => "Informações faltando"], 400);
+    if ($pedido["nome"] == "") resposta(["erro" => "Nome vazio"], 400);
+    if (!is_numeric($pedido["nota1"]) || !is_numeric($pedido["nota2"])) resposta(["erro" => "Notas devem ser um número"], 400);
 }
 
 ?>
---
+++ trabalho031126/server.php
@@ -0,0 +1,5 @@
+<?php
+
+declare(strict_types=1);
+require_once('func.php');
+header("Content-type: application/json;charset=UTF-8");
